a908a643457aacfa8fae2c70f2788849cd489d22
AlfredoRuiz-CS <alfreruiz2210@gmail.com>
2024-03-16 03:07:08 -0500

    style fixes to home

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -8,16 +8,13 @@ const Home = () => {
       <div className="flex h-screen flex-col overflow-y-hidden bg-bgwhite bg-gradient-to-b from-logoblue via-bgwhite to-bgwhite font-poppins text-black">
         <Header />
         {/* Page Content */}
-        <div className=" flex h-screen flex-col items-center text-pretty bg-transparent pt-[15rem] text-center">
-          <h1 className="font-jua flex w-[80rem] flex-row text-pretty text-center text-[5rem]">
+        <div className=" flex h-screen flex-col items-center text-pretty bg-transparent pt-[10rem] text-center">
+          <h1 className="font-jua flex w-[80rem] flex-row text-pretty text-center text-[5rem] ">
             Shop at ShastaMart for all your produce needs!
           </h1>
           {/* Buttons */}
-          <div className="flex flex-row gap-6">
-            <Button asChild className="" size="lg">
-              <a href="/register">Register</a>
-            </Button>
-            <Button
+          <div className="flex flex-row gap-6 mb-12 mt-6">
+          <Button
               asChild
               variant="outline"
               className="border-primary bg-transparent"
@@ -25,11 +22,14 @@ const Home = () => {
             >
               <a href="/login">Login</a>
             </Button>
+            <Button asChild className="" size="lg">
+              <a href="/register">Register</a>
+            </Button>
           </div>
         </div>
         <div className="mx-[15rem] flex flex-row justify-around gap-10 pb-10">
           {/* Bus Card */}
-          <div className="flex h-[35rem] w-[45rem] flex-col rounded-2xl bg-[#d9d9d9]">
+          <div className="flex h-[23rem] w-[45rem] flex-col rounded-2xl bg-[#d9d9d9]">
             <img src="./assets/bus.svg" className="h-[15rem]" />
             <ul className="list-disc pl-32 pt-5 text-2xl">
               <li>Fast free delivery to your address, on us.</li>
@@ -38,7 +38,7 @@ const Home = () => {
             </ul>
           </div>
           {/* Carrot Card */}
-          <div className="flex h-[35rem] w-[45rem] flex-col rounded-2xl bg-[#d9d9d9]">
+          <div className="flex h-[23rem] w-[45rem] flex-col rounded-2xl bg-[#d9d9d9]">
             <img src="./assets/carrot.svg" className="h-[15rem]" />
             <ul className="list-disc pl-32 pt-5 text-2xl">
               <li>Dozens of grocery items to choose from.</li>
