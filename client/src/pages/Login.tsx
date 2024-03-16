2a85f7dd34b65e86d2a8f8ad47025804e541e092
AlfredoRuiz-CS <alfreruiz2210@gmail.com>
2024-03-16 00:53:19 -0500

    initial login page setup

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,7 +1,33 @@
 import React from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import { useState } from 'react';
 
 const Login = () => {
-  return <div>Login</div>;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  return (
+      <div>
+        <Header />
+        <form className="">
+            <h1>Login</h1>
+            <input className=""
+              type="text"
+              placeholder="Email"
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+            />
+            <input className=""
+              type="text"
+              placeholder="Password"
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
+            />
+        </form>
+        <Footer />
+      </div>
+  );
 };
 
 export default Login;
