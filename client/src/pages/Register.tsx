5f0c2af408411297e466e5a28f29310644c3610a
SageCT <99381480+SageCT@users.noreply.github.com>
2024-03-18 00:21:13 -0500

    Orders.tsx finished, formatting changes to Products.tsx

--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -1,102 +1,104 @@
-import Header from "../components/Header";
-import Footer from "../components/Footer";
-import { useTypewriter, Cursor } from "react-simple-typewriter";
-import { useState } from "react";
-import { Button } from "../components/ui/button.tsx";
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import { useTypewriter, Cursor } from 'react-simple-typewriter';
+import { useState } from 'react';
+import { Button } from '../components/ui/button.tsx';
 
 const Register = () => {
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [email, setEmail] = useState("");
-  const [address, setAddress] = useState("");
-  const [phone, setPhone] = useState("");
-  const [password, setPassword] = useState("");
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [address, setAddress] = useState('');
+  const [phone, setPhone] = useState('');
+  const [password, setPassword] = useState('');
   const [isPasswordVisible, setIsPasswordVisible] = useState(false);
 
   const [message] = useTypewriter({
-    words: ["Great finds, Great buys"],
+    words: ['Great finds, Great buys'],
     loop: true,
   });
 
   return (
-    <div className="font-poppins flex min-h-screen flex-col overflow-x-hidden bg-bgwhite bg-gradient-to-b from-logoblue via-bgwhite to-bgwhite text-black">
-      <Header />
-      <form className="flex w-full flex-col items-center gap-5 py-5">
-        <h1 className="mb-5 font-jua text-8xl">Register</h1>
-        <a href="/login" className="mb-5 font-jua text-5xl text-darkblue">
-          Already have an account?
-        </a>
-        <input
-          className="mx-4 h-10 w-full max-w-md rounded-md border border-gray-300 px-4 focus:border-logoblue focus:ring-logoblue"
-          type="text"
-          placeholder="First name"
-          onChange={(e) => setFirstName(e.target.value)}
-          value={firstName}
-        />
-        <input
-          className="mx-4 h-10 w-full max-w-md rounded-md border border-gray-300 px-4 focus:border-logoblue focus:ring-logoblue"
-          type="text"
-          placeholder="Last name"
-          onChange={(e) => setLastName(e.target.value)}
-          value={lastName}
-        />
-        <input
-          className="mx-4 h-10 w-full max-w-md rounded-md border border-gray-300 px-4 focus:border-logoblue focus:ring-logoblue"
-          type="text"
-          placeholder="Email"
-          onChange={(e) => setEmail(e.target.value)}
-          value={email}
-        />
-        <input
-          className="mx-4 h-10 w-full max-w-md rounded-md border border-gray-300 px-4 focus:border-logoblue focus:ring-logoblue"
-          type="text"
-          placeholder="Address"
-          onChange={(e) => setAddress(e.target.value)}
-          value={address}
-        />
-        <input
-          className="mx-4 h-10 w-full max-w-md rounded-md border border-gray-300 px-4 focus:border-logoblue focus:ring-logoblue"
-          type="tel"
-          placeholder="Phone number"
-          onChange={(e) => {
-            if (e.target.value.match(/^\d*$/)) {
-              setPhone(e.target.value);
-            }
-          }}
-          value={phone}
-        />
-        <input
-          className="mx-4 h-10 w-full max-w-md rounded-md border border-gray-300 px-4 focus:border-logoblue focus:ring-logoblue"
-          type="password"
-          placeholder="Password"
-          onChange={(e) => setPassword(e.target.value)}
-          value={password}
-        />
-        <div className="mr-[310px] mt-[-14px] flex items-center">
+    <>
+      <div className="font-poppins flex min-h-screen flex-col overflow-x-hidden bg-bgwhite bg-gradient-to-b from-logoblue via-bgwhite to-bgwhite text-black">
+        <Header />
+        <form className="flex w-full flex-col items-center gap-5 py-5">
+          <h1 className="mb-5 font-jua text-8xl">Register</h1>
+          <a href="/login" className="mb-5 font-jua text-5xl text-darkblue">
+            Already have an account?
+          </a>
           <input
-            id="show-password"
-            type="checkbox"
-            className="mr-2"
-            onChange={(e) => setIsPasswordVisible(e.target.checked)}
-            checked={isPasswordVisible}
+            className="mx-4 h-10 w-full max-w-md rounded-md border border-gray-300 px-4 focus:border-logoblue focus:ring-logoblue"
+            type="text"
+            placeholder="First name"
+            onChange={(e) => setFirstName(e.target.value)}
+            value={firstName}
           />
-          <label htmlFor="show-password" className="select-none">
-            Show Password
-          </label>
-        </div>
-        <Button asChild className="" size="lg">
-          <p>Register</p>
-        </Button>
-      </form>
-      <h2 className="flex items-center justify-center font-jua text-[64px]">
-        {" "}
-        {message}
-        <span className="">
-          <Cursor />
-        </span>
-      </h2>
+          <input
+            className="mx-4 h-10 w-full max-w-md rounded-md border border-gray-300 px-4 focus:border-logoblue focus:ring-logoblue"
+            type="text"
+            placeholder="Last name"
+            onChange={(e) => setLastName(e.target.value)}
+            value={lastName}
+          />
+          <input
+            className="mx-4 h-10 w-full max-w-md rounded-md border border-gray-300 px-4 focus:border-logoblue focus:ring-logoblue"
+            type="text"
+            placeholder="Email"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+          />
+          <input
+            className="mx-4 h-10 w-full max-w-md rounded-md border border-gray-300 px-4 focus:border-logoblue focus:ring-logoblue"
+            type="text"
+            placeholder="Address"
+            onChange={(e) => setAddress(e.target.value)}
+            value={address}
+          />
+          <input
+            className="mx-4 h-10 w-full max-w-md rounded-md border border-gray-300 px-4 focus:border-logoblue focus:ring-logoblue"
+            type="tel"
+            placeholder="Phone number"
+            onChange={(e) => {
+              if (e.target.value.match(/^\d*$/)) {
+                setPhone(e.target.value);
+              }
+            }}
+            value={phone}
+          />
+          <input
+            className="mx-4 h-10 w-full max-w-md rounded-md border border-gray-300 px-4 focus:border-logoblue focus:ring-logoblue"
+            type="password"
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+          />
+          <div className="mr-[310px] mt-[-14px] flex items-center">
+            <input
+              id="show-password"
+              type="checkbox"
+              className="mr-2"
+              onChange={(e) => setIsPasswordVisible(e.target.checked)}
+              checked={isPasswordVisible}
+            />
+            <label htmlFor="show-password" className="select-none">
+              Show Password
+            </label>
+          </div>
+          <Button asChild className="" size="lg">
+            <p>Register</p>
+          </Button>
+        </form>
+        <h2 className="flex items-center justify-center font-jua text-[64px]">
+          {' '}
+          {message}
+          <span className="">
+            <Cursor />
+          </span>
+        </h2>
+      </div>
       <Footer />
-    </div>
+    </>
   );
 };
 
