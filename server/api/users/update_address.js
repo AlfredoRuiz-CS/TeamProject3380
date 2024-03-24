ffda86b9c676e0b3815732f8f796bc297006b3ab
SageCT <99381480+SageCT@users.noreply.github.com>
2024-03-23 20:27:52 -0500

    Added more functions for serverless

--- server/api/users/update_address.js
+++ server/api/users/update_address.js
@@ -2,30 +2,31 @@ const userController = require("../controllers/userController");
 const http = require("http");
 const { dbInitializer } = require("./config/db");
 const userController = require("./controllers/userController");
-const { pool } = require("../config/db");
+const userModel = require("../models/userModel");
+const jwt = require("jsonwebtoken");
 
 module.exports = async (req, res) => {
   try {
-    const body = await getRequestBody(req);
-    const { email, oldPassword, newPassword } = body;
+    const body = getRequestBody(req);
+    const { email, streetAddress, city, state, zipcode } = body;
 
-    const updatePassword = await userModel.updateUserPassword(
+    const updateUserAddress = await userModel.updateUserAddress(
       email,
-      oldPassword,
-      newPassword
+      streetAddress,
+      city,
+      state,
+      zipcode
     );
 
     res.writeHead(201, { "Content-Type": "application/json" });
     res.end(
-      JSON.stringify({
-        message: `Successfully updated password for - ${email}`,
-      })
+      JSON.stringify({ message: `Successfully updated phone for - ${email}` })
     );
   } catch (error) {
     res.writeHead(500, { "Content-Type": "application/json" });
     res.end(
       JSON.stringify({
-        status: "Failed to update password",
+        status: "Failed to update address",
         error: error.message,
       })
     );
--- server/api/users/update_phone.js
+++ server/api/users/update_phone.js
@@ -2,30 +2,25 @@ const userController = require("../controllers/userController");
 const http = require("http");
 const { dbInitializer } = require("./config/db");
 const userController = require("./controllers/userController");
-const { pool } = require("../config/db");
+const userModel = require("../models/userModel");
+const jwt = require("jsonwebtoken");
 
 module.exports = async (req, res) => {
   try {
-    const body = await getRequestBody(req);
-    const { email, oldPassword, newPassword } = body;
+    const body = getRequestBody(req);
+    const { email, newPhone } = body;
 
-    const updatePassword = await userModel.updateUserPassword(
-      email,
-      oldPassword,
-      newPassword
-    );
+    const updatePhone = await userModel.updateUserPhone(email, newPhone);
 
     res.writeHead(201, { "Content-Type": "application/json" });
     res.end(
-      JSON.stringify({
-        message: `Successfully updated password for - ${email}`,
-      })
+      JSON.stringify({ message: `Successfully updated phone for - ${email}` })
     );
   } catch (error) {
     res.writeHead(500, { "Content-Type": "application/json" });
     res.end(
       JSON.stringify({
-        status: "Failed to update password",
+        status: "Failed to update phone number",
         error: error.message,
       })
     );
