6799b33f0127c780807fca75f5a3228127f70e4d
SageCT <99381480+SageCT@users.noreply.github.com>
2024-03-23 20:14:44 -0500

    created template for updating serverless fucntions

---
+++ server/api/users/template.js
@@ -0,0 +1,6 @@
+const userController = require("../controllers/userController");
+const http = require("http");
+const { dbInitializer } = require("./config/db");
+const userController = require("./controllers/userController");
+
+module.exports = async (req, res) => {};
