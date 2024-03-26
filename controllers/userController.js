efa6a94c103462c0ba4b003191db236f446d712c
AlfredoRuiz-CS <alfreruiz2210@gmail.com>
2024-03-26 04:06:19 -0500

    sending registration data

--- controllers/userController.js
+++ controllers/userController.js
@@ -40,7 +40,7 @@ const registerAuth = async (req, res) => {
     const token = createToken(user.email);
 
     res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.end(JSON.stringify({ email, fName, lName, phoneNumber, streetAddress, city, state, zipcode, token, accountType }));
+    res.end(JSON.stringify({ email: user.email, fName: fName, lName: lName, phoneNumber: phoneNumber, streetAddress: streetAddress, city: city, state: state, zipcode: zipcode, token, accountType: user.accountType }));
   } catch (error) {
     if (!res.headersSent) {
       res.writeHead(400, { 'Content-Type': 'application/json' });
