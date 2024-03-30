2c87bb75ddb05b5c40944ac947271ede326f0ad9
AlfredoRuiz-CS <alfreruiz2210@gmail.com>
2024-03-30 04:22:31 -0500

    added insertion for product

--- models/productModel.js
+++ models/productModel.js
@@ -8,12 +8,96 @@ async function getAllProducts() {
 
         return rows;
 
-    } catch (error) {
+    } catch (error){
+        console.log(error);
+        throw error;
+    }
+}
+
+async function insertProduct(connection, productInfo){
+    try {
+        const [result] = await connection.execute(`
+        INSERT INTO product (productName, productDesc, productPrice, stockQuantity, categoryID, image, supplier, supplierStock, portion)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+        `, [
+            productInfo.productName,
+            productInfo.productDesc,
+            productInfo.productPrice,
+            productInfo.stockQuantity,
+            productInfo.categoryID,
+            productInfo.image,
+            productInfo.supplier,
+            productInfo.supplierStock,
+            productInfo.portion
+        ]);
+
+        return result.insertId;
+
+    } catch (error){
+        console.log(error);
+        throw error;
+
+    } 
+}
+
+async function insertNutritionFacts(connection, productID, nutritionFacts){
+    try {
+        const [result] = await connection.execute(`
+        INSERT INTO nutritionFacts (productID, servingSize, servingsPerContainer, calories, totalFat, cholesterol, sodium, totalCarbohydrate, dietaryFiber, sugars, protein, potassium, vitaminA, vitaminC, vitaminD, vitaminE, calcium, iron)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+        `, [
+            productID,
+            nutritionFacts.servingSize,
+            nutritionFacts.servingsPerContainer,
+            nutritionFacts.calories,
+            nutritionFacts.totalFat,
+            nutritionFacts.cholesterol,
+            nutritionFacts.sodium,
+            nutritionFacts.totalCarbohydrate,
+            nutritionFacts.dietaryFiber,
+            nutritionFacts.sugar,
+            nutritionFacts.protein,
+            nutritionFacts.potassium,
+            nutritionFacts.vitaminA,
+            nutritionFacts.vitaminC,
+            nutritionFacts.vitaminD,
+            nutritionFacts.vitaminE,
+            nutritionFacts.calcium,
+            nutritionFacts.iron
+        ]);
+
+        return result;
+
+    } catch (error){
+        console.log(error);
+        throw error;
+    }
+}
+
+async function insertShippingDetails(connection, productID, shippingDetails){
+    try {
+        const [result] = await connection.execute(`
+        INSERT INTO shippingDetails (productID, dimensionsLength, dimensionsWidth, dimensionsHeight, weight)
+        VALUES (?, ?, ?, ?, ?)
+        `, [
+            productID,
+            shippingDetails.dimensionsLength,
+            shippingDetails.dimensionsWidth,
+            shippingDetails.dimensionsHeight,
+            shippingDetails.weight
+        ]);
+
+        return result;
+
+    } catch (error){
         console.log(error);
         throw error;
     }
 }
 
 module.exports = {
-    getAllProducts
+    getAllProducts,
+    insertProduct,
+    insertNutritionFacts,
+    insertShippingDetails
 }
