9914ad8fa9a9f574eca1388602f3de997da3ddb8
AlfredoRuiz-CS <alfreruiz2210@gmail.com>
2024-04-02 22:27:53 -0500

    added debugging statements

--- api/users/template.js
+++
@@ -1,6 +0,0 @@
-const userController = require("../controllers/userController");
-const http = require("http");
-const { dbInitializer } = require("./config/db");
-const userController = require("./controllers/userController");
-
-module.exports = async (req, res) => {};
