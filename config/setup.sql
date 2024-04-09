5b3750e0b777948f0f7c1d3294b6325d518006da
AlfredoRuiz-CS <alfreruiz2210@gmail.com>
2024-04-09 03:10:53 -0500

    minor sql changes

--- config/setup.sql
+++ config/setup.sql
@@ -20,7 +20,7 @@ CREATE TABLE product(
 
 CREATE TABLE employee(
     email varchar(100) PRIMARY KEY,
-    fname varchar(100) NOT NULL,
+    fName varchar(100) NOT NULL,
     lName varchar(100) NOT NULL,
     jobTitle varchar(50),
     phoneNumber varchar(10),
@@ -47,8 +47,7 @@ CREATE TABLE customer(
 CREATE TABLE membership(
     membershipID int PRIMARY KEY AUTO_INCREMENT,
     customerEmail varchar(100),
-    membershipType varchar(50),
-    membershipStatus varchar(50),
+    membershipStatus boolean,
     startDate date,
     endDate date,
     renewalDate date,
