06d6c16ef2f001b8bb464564e123b997555f4ab3
Chinedu Okafor <chin02.okafor@gmail.com>
2024-04-20 02:20:55 -0500

    added routes

---
+++ api/shipping/getShippingById.js
@@ -0,0 +1,18 @@
+const ShippingController = require("../../controllers/shippingController");
+const { setCorsHeaders } = require("../../lib/cors");
+
+module.exports = async (req, res) => {
+    setCorsHeaders(req, res);
+    if (req.method === "OPTIONS") {
+        res.writeHead(204);
+        res.end();
+        return;
+    }
+
+    if (req.method === 'GET') {
+        await ShippingController.getShippinfInfoById(req, res);
+    } else {
+        res.writeHead(404, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({ message: 'Route Not Found' }));
+    }
+};
