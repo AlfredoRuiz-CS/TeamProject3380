e44f64e7594c46955700285aaa57fa9ad049315d
AlfredoRuiz-CS <alfreruiz2210@gmail.com>
2024-04-20 23:55:37 -0500

    slight change to membership

--- models/memberModel.js
+++ models/memberModel.js
@@ -64,7 +64,7 @@ async function getMembershipStatus(customerEmail) {
         const [rows] = await pool.query(`
         SELECT *
         FROM membership
-        WHERE customerEmail = ?`, [customerEmail]);
+        WHERE customerEmail = ?, membershipStatus = 1`, [customerEmail]);
         if (!rows) throw new Error("Couldn't find membership");
 
         let membershipStatus = "None";
